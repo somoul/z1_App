2cabba6b484c57019b7620a915e3c9ed1b45b42d
somoul <somoul168168168@gmail.com>
2022-08-05 10:12:44 +0700

    update firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'configs/router/auto_route.gr.dart';
+import 'firebase_options.dart';
 
-void main() {
-  // WidgetsFlutterBinding.ensureInitialized();
-  // await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   // SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky,
   //     overlays: []);
   // SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
