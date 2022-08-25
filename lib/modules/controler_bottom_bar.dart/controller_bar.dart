bdb8efbfebe109ec168dbd58ccd10e3293d92f14
somoul <somoul168168168@gmail.com>
2022-08-25 15:10:34 +0700

    update

--- lib/modules/controler_bottom_bar.dart/controller_bar.dart
+++ lib/modules/controler_bottom_bar.dart/controller_bar.dart
@@ -1,6 +1,4 @@
 import 'package:get/get.dart';
-import 'package:get/get_state_manager/get_state_manager.dart';
-
 class BottomBarController extends GetxController{
    final selecClickButtomNavigetionBar=0.obs;
       final selecClickButtomNavigetionBarback=0.obs;
--- lib/utils/mini_show_app/mini_app_screen.dart
+++ lib/utils/mini_show_app/mini_app_screen.dart
@@ -19,17 +19,28 @@ class MiniAppScreen extends StatefulWidget {
 
 class _MiniAppScreenState extends State<MiniAppScreen> {
   final _homeController = Get.put(HomeController());
+   @override
+   void initState() {
+     super.initState();
+     // Enable virtual display.
+     setState(() {
+       if (Platform.isAndroid) WebView.platform = AndroidWebView();
+  
+     });
+   }
 
-  @override
-  void initState() {
-    super.initState();
-    // Enable virtual display.
-    if (Platform.isAndroid) WebView.platform = AndroidWebView();
-         _homeController.isLodingminiApp.value = true;
-    Timer(const Duration(seconds: 5), () {
-      _homeController.isLodingminiApp.value = false;
-    });
-  }
+  // @override
+  // void initState() {
+
+  //   // Enable virtual display.
+  //   if (Platform.isAndroid) WebView.platform = AndroidWebView();
+  //   //      _homeController.isLodingminiApp.value = true;
+  //   //      debugPrint('====widget.linkApp: ${widget.linkApp}');
+  //   // Timer(const Duration(seconds: 2), () {
+  //   //   _homeController.isLodingminiApp.value = false;
+  //   // });
+  //       super.initState();
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -46,23 +57,27 @@ class _MiniAppScreenState extends State<MiniAppScreen> {
               Navigator.pop(context);
             }),
       ),
-      body: Obx(
-        () => _homeController.isLodingminiApp.value
-            ? const Center(
-                child: Padding(
-                  padding: EdgeInsets.only(bottom: 100),
-                  child: CircularProgressIndicator(
-                    value: null,
-                    strokeWidth: 5.0,
-                    color: Color.fromARGB(255, 101, 201, 226),
-                  ),
-                ),
-              )
-            : WebView(
-                javascriptMode: JavascriptMode.unrestricted,
-                initialUrl: '${widget.linkApp}',
-              ),
-      ),
+      body:  WebView(
+       initialUrl:'https://online-quiz-75798.firebaseapp.com',
+     ),
+      
+        //  _homeController.isLodingminiApp.value
+        //     ? const Center(
+        //         child: Padding(
+        //           padding: EdgeInsets.only(bottom: 100),
+        //           child: CircularProgressIndicator(
+        //             value: null,
+        //             strokeWidth: 5.0,
+        //             color: Color.fromARGB(255, 101, 201, 226),
+        //           ),
+        //         ),
+        //       )
+        //     : 
+            // WebView(
+            //     javascriptMode: JavascriptMode.unrestricted,
+            //     initialUrl:'${widget.linkApp}',
+            //   ),
+     
     );
   }
 }
